781c97fcdc0b69b9c793bb7ac4c64f5ac30b5aac
Jobayer Hossen <jobayerhossen64@gmail.com>
2024-10-02 03:04:27 +0600

    Refactor import statements and schema names in public and store directories

--- src/db/other/query/query.js
+++ src/db/other/query/query.js
@@ -29,5 +29,40 @@ export async function selectBuyer(req, res, next) {
 	handleResponse({ promise: buyerPromise, res, next, ...toast });
 }
 
+//public category
 
+export async function selectCategory(req, res, next) {
+	const categoryPromise = db
+		.select({
+			value: publicSchema.category.uuid,
+			label: publicSchema.category.name,
+		})
+		.from(publicSchema.category)
+		.orderBy(publicSchema.category.name);
 
+	const toast = {
+		status: 200,
+		type: 'select',
+		message: `Category selected`,
+	};
+	handleResponse({ promise: categoryPromise, res, next, ...toast });
+}
+
+//public article
+
+export async function selectArticle(req, res, next) {
+	const articlePromise = db
+		.select({
+			value: publicSchema.article.uuid,
+			label: publicSchema.article.name,
+		})
+		.from(publicSchema.article)
+		.orderBy(publicSchema.article.name);
+
+	const toast = {
+		status: 200,
+		type: 'select',
+		message: `Article selected`,
+	};
+	handleResponse({ promise: articlePromise, res, next, ...toast });
+}
--- src/db/other/route.js
+++ src/db/other/route.js
@@ -5,9 +5,15 @@ import * as otherOperations from './query/query.js';
 const otherRouter = Router();
 
 //public buyer
-
 otherRouter.get('/buyer/value/label', otherOperations.selectBuyer);
 
+//public category
+otherRouter.get('/category/value/label', otherOperations.selectCategory);
+
+//public article
+otherRouter.get('/article/value/label', otherOperations.selectArticle);
+
+//public buyer swagger route
 const pathBuyer = {
 	'/other/buyer/value/label': {
 		get: {
@@ -41,8 +47,80 @@ const pathBuyer = {
 	},
 };
 
+//public category swagger route
+
+const pathCategory = {
+	'/other/category/value/label': {
+		get: {
+			operationId: 'selectCategory',
+			tags: ['other'],
+			summary: 'Select Category',
+			description: 'Select Category',
+			responses: {
+				200: {
+					description: 'Returns a all categories.',
+					content: {
+						'application/json': {
+							schema: {
+								type: 'object',
+								properties: {
+									value: {
+										type: 'string',
+										example: '2ggcphnwHGzEUGy',
+									},
+									label: {
+										type: 'string',
+										example: 'category 1',
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
+
+//public article swagger route
+
+const pathArticle = {
+	'/other/article/value/label': {
+		get: {
+			operationId: 'selectArticle',
+			tags: ['other'],
+			summary: 'Select Article',
+			description: 'Select Article',
+			responses: {
+				200: {
+					description: 'Returns a all articles.',
+					content: {
+						'application/json': {
+							schema: {
+								type: 'object',
+								properties: {
+									value: {
+										type: 'string',
+										example: '2ggcphnwHGzEUGy',
+									},
+									label: {
+										type: 'string',
+										example: 'article 1',
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+};
+
 export const pathOther = {
 	...pathBuyer,
+	...pathCategory,
+	...pathArticle,
 };
 
 export const tagOther = [

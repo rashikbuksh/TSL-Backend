20128335d61e351ce006016cf7b7cd18c5d0fb44
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-03 10:28:37 +0600

    Refactor CORS whitelist URLs

--- src/util/cors.js
+++ src/util/cors.js
@@ -1,33 +1,26 @@
 import cors from 'cors';
 
 const whitelist = [
-	// * VPS
-	'http://147.79.70.48:4010',
-	'http://147.79.70.48:4015',
-	'http://www.fortunezip.com',
-	'http://fortunezip.com',
-	'https://www.fortunezip.com',
-	'https://fortunezip.com',
 	//* FZL H/O
 	'http://103.147.163.46:3005',
 	'http://103.147.163.46:4010',
 	'http://103.147.163.46:3000',
-	'http://103.147.163.46:4015',
-	'http://103.147.163.46:4016',
+	'http://103.147.163.46:4025',
+	'http://103.147.163.46:4026',
 
 	//* Development
 	'http://localhost:3005',
 	'http://localhost:4010',
 	'http://localhost:3000',
-	'http://localhost:4015',
-	'http://localhost:4016',
+	'http://localhost:4025',
+	'http://localhost:4026',
 
 	//* Office Server PC
 	'http://192.168.10.154:3005',
 	'http://192.168.10.154:4010',
 	'http://192.168.10.154:3000',
-	'http://192.168.10.154:4015',
-	'http://192.168.10.154:4016',
+	'http://192.168.10.154:4025',
+	'http://192.168.10.154:4026',
 
 	//* RBR LAPTOP
 	'http://192.168.10.78:4010',

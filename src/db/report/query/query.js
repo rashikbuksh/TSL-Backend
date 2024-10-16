d1c1e50d2969c21444d84b924509a531d631107c
Rashik Buksh <rashikbuksh@gmail.com>
2024-10-16 13:04:55 +0600

    Refactor storeVendorWiseMaterialReport query to include avg_convention_rate in the SELECT clause

--- src/db/report/query/query.js
+++ src/db/report/query/query.js
@@ -41,49 +41,25 @@ export async function storeVendorWiseMaterialReport(req, res, next) {
                 SELECT 
 				   	vendor.uuid as vendor_uuid,
 					vendor.name as vendor_name,
-					m.name,
-					m.unit,
 					material_quantity_total.total_quantity::float8,
 					material_quantity_total.price_bdt::float8,
-					avg.avg_convention_rates::float8,
-					ROUND(material_quantity_total.price_bdt::numeric / material_quantity_total.total_quantity::numeric, 4)::float8 as avg_price_per_unit_bdt,
-					ROUND(material_quantity_total.price_bdt::numeric / avg.avg_convention_rates::numeric, 4)::float8 as price_usd,
-					(ROUND(material_quantity_total.price_bdt::numeric / material_quantity_total.total_quantity::numeric, 4) / avg.avg_convention_rates)::float8 as avg_price_per_unit_usd,
-					lc.number as lc_number,
-					lc.created_at as lc_created_at,
-					m.created_at as material_created_at
+					material_quantity_total.avg_convention_rate::float8
 				FROM
 					store.vendor
 				LEFT JOIN (
 					SELECT 
-						re.material_uuid,
 						r.vendor_uuid,
-						r.lc_uuid,
 						SUM(re.quantity) as total_quantity,
-						SUM(re.price * r.convention_rate) as price_bdt
+						SUM(re.price * r.convention_rate) as price_bdt,
+						SUM(CASE WHEN r.convention_rate > 1 THEN r.convention_rate END ) / COUNT (CASE WHEN r.convention_rate > 1 THEN r.convention_rate END) as avg_convention_rate
 					FROM
 						store.receive_entry re
 						LEFT JOIN store.receive r ON re.receive_uuid = r.uuid
 					WHERE 
 						re.created_at BETWEEN ${start_date}::TIMESTAMP AND ${end_date}::TIMESTAMP + interval '23 hours 59 minutes 59 seconds'
 					GROUP BY
-						re.material_uuid, r.vendor_uuid, r.lc_uuid
+						r.vendor_uuid
 				) material_quantity_total ON vendor.uuid = material_quantity_total.vendor_uuid
-				LEFT JOIN 
-					store.material m ON material_quantity_total.material_uuid = m.uuid
-				LEFT JOIN (
-					SELECT
-						ROUND(SUM(r.convention_rate) / COUNT(r.convention_rate), 4)::float8 AS avg_convention_rates,
-						re.material_uuid
-					FROM store.receive r
-					LEFT JOIN store.receive_entry re ON r.uuid = re.receive_uuid
-					WHERE
-						re.created_at IS NOT NULL
-						AND re.created_at BETWEEN ${start_date}::TIMESTAMP AND ${end_date}::TIMESTAMP + interval '23 hours 59 minutes 59 seconds' AND r.convention_rate > 1
-					GROUP BY
-						re.material_uuid
-				) avg ON m.uuid = avg.material_uuid
-				LEFT JOIN commercial.lc ON material_quantity_total.lc_uuid = commercial.lc.uuid 
             `;
 
 	const resultPromise = db.execute(query);
